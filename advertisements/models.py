595dc92d7d350213da935a95f354c4bd305931d5
Izz <elizavetakirzanova@gmail.com>
2023-08-19 15:43:49 +0300

    HW 7

--- advertisements/models.py
+++ advertisements/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField(max_length=80)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    auction = models.BooleanField(help_text="Отметьте, если торг уместен")
+    created_at = models.DateTimeField(auto_now = True)
+    updated_at = models.DateTimeField(auto_now_add = True)
+    
+    class Meta: 
+        db_table = 'advertisements'
+
+    def __str__(self): 
+        return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
